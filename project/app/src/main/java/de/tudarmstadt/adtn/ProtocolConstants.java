cfe2f7538a6a0ba6c1f0f9c965000cc56b41567e
megfault <ana@megfau.lt>
2016-01-17 01:35:43 +0100

    add private constructor

--- project/app/src/main/java/de/tudarmstadt/adtn/ProtocolConstants.java
+++ project/app/src/main/java/de/tudarmstadt/adtn/ProtocolConstants.java
@@ -8,4 +8,7 @@ public class ProtocolConstants {
     public final static int MESSAGE_HEADER_SIZE = 1;
     public final static int MAX_MESSAGE_CONTENT_SIZE = 1453;
     public final static int MAX_MESSAGE_SIZE = MESSAGE_HEADER_SIZE + MAX_MESSAGE_CONTENT_SIZE;
+
+    private ProtocolConstants() {}
+
 }
