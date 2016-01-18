ffd862b0c1f79d2f9e8de9670f7ff573d7c77137
megfault <ana@megfau.lt>
2016-01-18 16:26:56 +0100

    reorder elements

--- project/app/src/main/java/de/tudarmstadt/adtn/preferences/IPreferences.java
+++ project/app/src/main/java/de/tudarmstadt/adtn/preferences/IPreferences.java
@@ -7,26 +7,32 @@ public interface IPreferences extends de.tudarmstadt.adtn.genericpreferences.IPr
 
     // Sending pool send interval
     int DEFAULT_SENDING_POOL_SEND_INTERVAL = 10;
-    int getSendingPoolSendInterval();
-    void setSendingPoolSendInterval(int sendInterval);
-
     // Sending pool refill threshold
     int DEFAULT_SENDING_POOL_REFILL_THRESHOLD = 10;
-    int getSendingPoolRefillThreshold();
-    void setSendingPoolRefillThreshold(int refillThreshold);
-
     // Sending pool batch size
     int DEFAULT_SENDING_POOL_BATCH_SIZE = 10;
-    int getSendingPoolBatchSize();
-    void setSendingPoolBatchSize(int batchSize);
-
     // Auto-join ad-hoc network
     boolean DEFAULT_AUTO_JOIN_AD_HOC_NETWORK = true;
-    boolean getAutoJoinAdHocNetwork();
-    void setAutoJoinAdHocNetwork(boolean autoJoin);
-
     // Show help buttons
     boolean DEFAULT_SHOW_HELP_BUTTONS = true;
+
+    int getSendingPoolSendInterval();
+
+    void setSendingPoolSendInterval(int sendInterval);
+
+    int getSendingPoolRefillThreshold();
+
+    void setSendingPoolRefillThreshold(int refillThreshold);
+
+    int getSendingPoolBatchSize();
+
+    void setSendingPoolBatchSize(int batchSize);
+
+    boolean getAutoJoinAdHocNetwork();
+
+    void setAutoJoinAdHocNetwork(boolean autoJoin);
+
     boolean getShowHelpButtons();
+
     void setShowHelpButtons(boolean showHelpButtons);
 }
